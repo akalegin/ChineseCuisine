fc81613171c73a3ecf9bde2bee4232da6ae8a87d
akalegin <dronkalegin@yandex.ru>
2015-01-06 21:36:33 +0300

    Application with pictures and dummy fragments

--- app/src/main/java/com/example/andrey/chinesecuisine/MainActivity.java
+++ app/src/main/java/com/example/andrey/chinesecuisine/MainActivity.java
@@ -4,13 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE = "com.example.andrey.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +28,15 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void showDish(View view) {
